7327cb5de958caa452970336b43ff41f6884ab9c
Matt McFarland <matthew.r.mcfarland.16@dartmouth.edu>
2016-02-15 21:31:51 -0500

    profiled beer and reviewer distribution

--- profile.m
+++ profile.m
@@ -7,17 +7,27 @@ beer_csv = 'data/csv/beer_combined.csv'
 
 format_spec = '%s , %d';
 
+% show reviews profile
 reviewer_data = readtable(reviewers_csv);
+sorted_reviewers = sort(reviewer_data{:,2},1,'descend');
+total_reviewers = sum(sorted_reviewers);
 figure();
-hist(reviewer_data{:,2})
-title('reviewer histogram')
+[ax, p1, p2] = plotyy( cumsum(sorted_reviewers) ./ total_reviewers,'b',...
+                        sorted_reviewers, 'r');
+title('review cumulation')
+xlabel('reviewer')
+ylabel(ax(1),'cumulative portion of reviews')
+ylabel(ax(2),'review contributions')
 
+% show beers profile
 beer_data = readtable(beer_csv);
+sorted_beer = sort(beer_data{:,2},1,'descend');
+total_beer = sum(sorted_beer);
 figure();
-hist(beer_data{:,2})
-title('beer histogram')
+[ax, p1, p2] = plotyy(cumsum(sorted_beer) ./ total_beer,'b',...
+                      sorted_beer,'r');
+title('beer cumulation')
+xlabel('beer')
+ylabel(ax(1),'cumulative portion of reviews')
+ylabel(ax(2),'beer contribution to total')
 
-breweries_data = readtable(breweries_csv);
-figure();
-hist(breweries_data{:,2})
-title('breweries histogram')
--- profile_data.py
+++ profile_data.py
@@ -9,7 +9,7 @@ import os.path
 # 0			 1 				2			3				4			5					6				7			8				9			10 		11		12
 # brewery_id,brewery_name,review_time,review_overall,review_aroma,review_appearance,review_profilename,beer_style,review_palate,review_taste,beer_name,beer_abv,beer_beerid
 
-json_dir = './json/'
+json_dir = './data/'
 brewery_dir = json_dir + 'breweries/'
 beers_dir = json_dir + 'beers/'
 reviewers_dir = json_dir + 'reviewers/'
@@ -38,9 +38,9 @@ def read_file(filename):
 				print 'line ' + str(j) + ' of file ' + filename
 
 			data = line.strip().split(',')
-			increment_key(brewery_name, data[1]) 	# count brewery occurence
+			increment_key(brewery_name, data[0]) 	# count brewery occurence
 			increment_key(reviewers, data[6]) 		# count reviewers occurence
-			increment_key(beers, data[10])			# count beers occurence	
+			increment_key(beers, data[12])			# count beers occurence	
 
 		with open(brewery_dir + suffix + '_brewery_names.json', 'w') as fp:
 			json.dump(brewery_name, fp)
